e7343e528c7e6ce70ff58a4b1fe849328e2348ce
ghersi cherifa <ghersi.cherifa@gmail.com>
2017-03-29 14:15:55 +0200

    feature register

--- client/app/users/services/users.service.ts
+++ client/app/users/services/users.service.ts
@@ -21,15 +21,15 @@ export class UsersService {
 
 
     signup(user: User): Observable<any> {
+      console.log('user',user);
         let backendURL = `${this._baseUrl}${environment.backend.endpoints.signup}` ;
-        return this.http.post(backendURL, user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post(backendURL, user).map((response: Response) => response.json());
     }
 
 
     // private helper methods
     private jwt() {
         // create authorization header with jwt token
-        console.log("getcurrenUser");
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         if (currentUser && currentUser.token) {
             let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
