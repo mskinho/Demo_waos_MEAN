4e906db5b5c130b2b6b433f24ccd16230e85a532
MOHAMED FARHAT <mohamed.farhat.09@gmail.com>
2017-04-25 23:01:35 +0200

    change back-end api for prod

--- client/environments/environment.prod.ts
+++ client/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     backend: {
         protocol: 'http',
-        host: '127.0.0.1',
-        port: '3000',
+        host: 'romeanet.weareopensource.me',
+        port: '80',
         endpoints: {
           signup: '/api/auth/signup',
           signin: '/api/auth/signin',
